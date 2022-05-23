5c8b0a4885ddc678779f937eb3d8103b62f41beb
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-05-23 11:12:56 +0300

    Cleaning the code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Store the imgages sources inside variables then inside an array
+// Store the images sources inside variables then inside an array
 const dice1_src = "./images/dice1.png";
 const dice2_src = "./images/dice2.png";
 const dice3_src = "./images/dice3.png";
@@ -7,25 +7,14 @@ const dice5_src = "./images/dice5.png";
 const dice6_src = "./images/dice6.png";
 const dices = [dice1_src, dice2_src, dice3_src, dice4_src, dice5_src, dice6_src];
 
+//All possible results
+const outcome = ["Draw!","&#128681 Play 1 wins!","&#128681 Play 2 wins!"];
+
 
 //returns a random integer between 0 and max (max excluded)
 function getRandomInt(max) {
    return Math.floor(Math.random() * max);
 }
-
-//Storing the random numbers in vairables
-//we need them later to check for the winner
-var rand_1 = getRandomInt(6);
-var rand_2 = getRandomInt(6);
-
-
-//Storing the two dices element in two variables
-let dice_1_element = document.getElementsByClassName("img1")[0];
-let dice_2_element = document.getElementsByClassName("img2")[0];
-
-//change the dices elements sources based on the result
-dice_1_element.src = dices[rand_1];
-dice_2_element.src = dices[rand_2];
 
 // Determine who wins
 function getWinner() {
@@ -40,14 +29,21 @@ function getWinner() {
     }
 }
 
-//All possible results
-const outcome = ["Draw!","&#128681 Play 1 wins!","&#128681 Play 2 wins!"];
+//Storing the random numbers in vairables
+//we need them later to check for the winner
+var rand_1 = getRandomInt(6);
+var rand_2 = getRandomInt(6);
+
+
+//change the dices elements sources based on the result
+document.getElementsByClassName("img1")[0].src = dices[rand_1];
+document.getElementsByClassName("img2")[0].src = dices[rand_2];
+
 
 //this function will display the result in the element tag passed to it
 function displayResult(tag_name){
    //calling the h1 element to display result
-    var element = document.getElementsByTagName(tag_name)[0];
-    element.innerHTML = outcome[getWinner()];
+    document.getElementsByTagName(tag_name)[0].innerHTML = outcome[getWinner()];
 }
 
 
